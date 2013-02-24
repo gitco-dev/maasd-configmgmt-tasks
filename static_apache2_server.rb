e882f960ea949006f2e3ee133b0c36624abd2e8f
gitco-dev <dev@gitco.de>
2013-02-24 13:55:30 +0100

    added CentOS support for task static_apache2_server

--- static_apache2_server.rb
+++ static_apache2_server.rb
@@ -2,37 +2,55 @@ PublicConfigtasks.create(
   :configtask_id => 0,
   :configname => "Static Apache2 Server", 
   :description => "This is a simple Apache2 Server",
-  :configtask => <<-EOF.unindent
-
+  :configtask => <<-'EOF'.unindent
+ 
   # GIT Version: $Id$
-
+ 
   task "config-static-apache2", sub {
-
+ 
+    \$content = "<html><body><h4><a href=\"http://www.rexify.org/\">
+                THIS IS A STATIC APACHE2, CONFIGURED VIA REX</a></h4></body></html>";
+ 
     if(is_debian) {
-
+ 
       install package => "apache2-mpm-worker";
-
+ 
       file "/var/www/index.html",
-        content => "<html><body><h4><a href=\"http://www.rexify.org/\">
-          THIS IS A STATIC APACHE2, CONFIGURED VIA REX</a></h4></body></html>",
+        content => \$content,
         mode    => 644;
-
+ 
       service apache2 => "ensure", "started";
-
+ 
     } elsif(is_suse) {
+ 
       if(operating_system_version =~ /^11/ ) {
+ 
         install package => "apache2-worker";
-
+ 
         file "/srv/www/htdocs/index.html",
-          content => "<html><body><h4><a href=\"http://www.rexify.org/\">
-            THIS IS A STATIC APACHE2, CONFIGURED VIA REX</a></h4></body></html>",
+          content => \$content,
           mode    => 644;
-
+ 
         service apache2 => "ensure", "started";
-
+ 
       }
+ 
+    } elsif(is_redhat) {
+ 
+      if(operating_system_version =~ /^6/ ) {
+ 
+        install package => "httpd";
+ 
+        file "/var/www/html/index.html",
+          content => \$content,
+          mode    => 644;
+ 
+        service httpd => "ensure", "started";
+ 
+      }
+ 
     }
-
+ 
   };
   EOF
 
